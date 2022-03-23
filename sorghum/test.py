e3cc6ea79cedcfef2b38c70b6df3d6540adf08c4
Brian Lee <b.lee@utexas.edu>
2022-03-23 11:44:34 +0900

    redefined dataloader parameter (increased batch size to 512)

--- sorghum/test.py
+++ sorghum/test.py
@@ -20,16 +20,14 @@ sys.path.append('./src/')
 from data import SorghumDataset
 
 # %%
-CHK_PATH = '/home/brian/github/kaggle_herbarium_2022/sorghum/tb_logs/version_1/checkpoints/epoch=2-step=104 copy.ckpt'
+CHK_PATH = '/home/brian/github/kaggle_herbarium_2022/sorghum/tb_logs/20220323_105943/epoch=18-val_loss=0.31.ckpt'
 
 # %%
 # Need to have caled self.save_hyperparameters() in model init for the below to work!
 model = SorghumLitModel.load_from_checkpoint(checkpoint_path=CHK_PATH) 
 
-print('='*90)
-
 test_dataset = SorghumDataset(csv_fullpath='test.csv', testset=True)
-dl_test = DataLoader(dataset=test_dataset, shuffle=False, batch_size=128, num_workers=16)
+dl_test = DataLoader(dataset=test_dataset, shuffle=False, batch_size=512, num_workers=16)
 
 trainer = Trainer(gpus=1)
 results = trainer.test(model=model, test_dataloaders=dl_test, verbose=True)
