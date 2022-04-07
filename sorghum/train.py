30b0b70b4b61a54eee9e11479c02bcd9c745d783
Brian Lee <b.lee@utexas.edu>
2022-04-07 21:13:09 +0900

    saved modified train.py for merging to main

--- sorghum/train.py
+++ sorghum/train.py
@@ -232,22 +232,22 @@ TRANSFORMS = {'train': A.Compose([
                 A.RandomRotate90(p=0.5),
                 A.ShiftScaleRotate(p=0.5),
                 A.HueSaturationValue(p=0.5),
-                A.OneOf([ # Including this improved performance from 0.725 to 0.730
-                    A.RandomBrightnessContrast(p=0.5),
-                    A.RandomGamma(p=0.5),
-                ], p=0.5),
-                A.OneOf([ # Decreased performance from 0.730 to 0.723
-                    A.Blur(p=0.1),
-                    A.GaussianBlur(p=0.1),
-                    A.MotionBlur(p=0.1),
-                ], p=0.1),
-                A.OneOf([ # Increased performance from 0.723 to 0.727
-                    A.GaussNoise(p=0.1),
-                    A.ISONoise(p=0.1),
-                    A.GridDropout(ratio=0.5, p=0.2),
-                    A.CoarseDropout(max_holes=16, min_holes=8, max_height=16, max_width=16, min_height=8, min_width=8, p=0.2)
-                ], p=0.2),
-               A.Normalize(IMAGENET_NORMAL_MEAN, IMAGENET_NORMAL_STD), # Turning this on obliterated performance (only for validation metrics)
+                # A.OneOf([ # Including this improved performance from 0.725 to 0.730
+                #     A.RandomBrightnessContrast(p=0.5),
+                #     A.RandomGamma(p=0.5),
+                # ], p=0.5),
+                # A.OneOf([ # Decreased performance from 0.730 to 0.723
+                #     A.Blur(p=0.1),
+                #     A.GaussianBlur(p=0.1),
+                #     A.MotionBlur(p=0.1),
+                # ], p=0.1),
+                # A.OneOf([ # Increased performance from 0.723 to 0.727
+                #     A.GaussNoise(p=0.1),
+                #     A.ISONoise(p=0.1),
+                #     A.GridDropout(ratio=0.5, p=0.2),
+                #     A.CoarseDropout(max_holes=16, min_holes=8, max_height=16, max_width=16, min_height=8, min_width=8, p=0.2)
+                # ], p=0.2),
+                A.Normalize(IMAGENET_NORMAL_MEAN, IMAGENET_NORMAL_STD),
                 ToTensorV2(), # np.array HWC image -> torch.Tensor CHW
             ]), # Try one where the normalization happens before colorjitter and channelshuffle -> not a good idea
 
@@ -260,7 +260,7 @@ TRANSFORMS = {'train': A.Compose([
 # Transforms above inspired by the following post:
 #   https://www.kaggle.com/code/pegasos/sorghum-pytorch-lightning-starter-training
 
-TB_NOTES = "3OneOf_OneCycleLR_2FCLayer1stLayer4096_BaseCase20220404_075755"
+TB_NOTES = "0OneOf_OneCycleLR_2FCLayer1stLayer4096_BaseCaseSelf"
 TB_NOTES += "_" + host_name
 
 # %%
