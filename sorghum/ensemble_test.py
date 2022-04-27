f8c401f9286429ab67610431d0834f771082555b
Brian Lee <b.lee@utexas.edu>
2022-04-28 08:19:35 +0900

    ensemble test code updated

--- sorghum/ensemble_test.py
+++ sorghum/ensemble_test.py
@@ -2,6 +2,7 @@
 import os
 import time
 import albumentations as A
+from datetime import datetime
 from pytorch_lightning import Trainer
 from torch.utils.data import DataLoader
 from albumentations.pytorch.transforms import ToTensorV2
@@ -17,15 +18,21 @@ TRANSFORM  = A.Compose([
 ])
 
 # %%
-ENSEMBLE_MODEL_PATH = ""
+ENSEMBLE_MODEL_PATH = "/home/brian/github/kaggle_herbarium_2022/sorghum/tb_logs/20220427_060458_Ensemble_20220427_060458_nipa2022-49703_DropoutRate0.5/epoch=49-val_loss=0.00172.ckpt"
 
 model = EnsembleModel.load_from_checkpoint(checkpoint_path=ENSEMBLE_MODEL_PATH)
+
+# Remove below for later runs
+# model.csv_header_written = False
+# model.csv_header_written = False
+# now = datetime.now().strftime('%Y%m%d_%H%M%S')
+# model.tests_result_csv_filename = 'test_result_{}.csv'.format(now)
 
 # %%
 start_time = time.time()
 
 test_dataset = SorghumDataset(csv_fullpath='test.csv', testset=True, transform=TRANSFORM)
-dl_test = DataLoader(dataset=test_dataset, shuffle=False, batch_size=32, num_workers=os.cpu_count())
+dl_test = DataLoader(dataset=test_dataset, shuffle=False, batch_size=42, num_workers=os.cpu_count())
 
 trainer = Trainer(gpus=1)
 results = trainer.test(model=model, dataloaders=dl_test, verbose=True)
